48be5417db0467bf44ab221b830bed64b83e297f
techmannih <manishchaudharyttt@gmail.com>
2023-07-23 10:30:32 +0530

    new

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-const closeButton = document.querySelector('.close-nav');
-const openButton = document.querySelector('.open-nav');
-const nav = document.querySelector('.nav');
+// const closeButton = document.querySelector('.close-nav');
+// const openButton = document.querySelector('.open-nav');
+// const nav = document.querySelector('.nav');
 
-closeButton.addEventListener("click", () => {
-    nav.classList.remove('navigation-open');
-}); 
+// closeButton.addEventListener("click", () => {
+//     nav.classList.remove('navigation-open');
+// }); 
 
-openButton.addEventListener("click", () => {
-    nav.classList.add('navigation-open');
-}); 
+// openButton.addEventListener("click", () => {
+//     nav.classList.add('navigation-open');
+// }); 
+
+
+//////////////
+//////////////
+// javascript
+
+const get = element => document.getElementById(element);
+
+let open = get("menu-btn");
+let nav = get("nav");
+let exit = get("exit-btn");
+
+open.addEventListener('click', () => {
+    nav.classList.add('open-nav');
+})
+
+exit.addEventListener('click', () => {
+    nav.classList.remove('open-nav');
+})
